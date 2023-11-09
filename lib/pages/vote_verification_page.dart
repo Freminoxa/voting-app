a6e8531b0e692d9c9fa24aaf19f9c007b4782d57
Paulmuia <paulmuia7511@gmail.com>
2023-11-09 15:13:07 +0300

    changed color

--- lib/pages/vote_verification_page.dart
+++ lib/pages/vote_verification_page.dart
@@ -10,7 +10,7 @@ class VoteVerificationPage extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         title: Text('Vote Verification'),
-        backgroundColor: Colors.blue,
+        backgroundColor: Color.fromARGB(255, 6, 100, 9),
       ),
       body: Center(
         child: Column(
@@ -25,7 +25,18 @@ class VoteVerificationPage extends StatelessWidget {
                   
                   'lib/assets/vstar.jpg'),
               ),
-              Text('Your vote has been verified',style: TextStyle(fontSize: 20,color: Colors.green,fontWeight: FontWeight.w700),)
+              Text('Your vote has been verified',style: TextStyle(fontSize: 20,color: Colors.green,fontWeight: FontWeight.w700),),
+              SizedBox(height: 60,),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 80,vertical: 20),
+                height: 60,
+                width: double.maxFinite,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  color: Color.fromARGB(255, 6, 100, 9),
+                ),
+                child: Center(child: Text('Close',style: TextStyle(color: Colors.white,fontSize: 30),)),
+              )
           ],
         ),
       ),
